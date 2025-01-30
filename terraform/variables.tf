6ed0741f21c9c1f1ca05b82f0d773c3a5c176f7b
perhoff <per.hoff@soprasteria.com>
2025-01-30 14:49:49 +0100

    Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "rg_name" {
 
 variable "rg_location" {
   type        = string
-  description = "Azure region for Right Cloud resources2"
+  description = "Azure region for Right Cloud resources 2"
   default     = "West Europe"
 }
 
